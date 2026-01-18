48bdcce0ac97d05e48e65204722a6e27fea3f8e1
g3lasio <gelasio@chyrris.com>
2026-01-18 03:15:57 +0000

    Fix: Add @ alias resolution to metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,13 @@
 // Learn more https://docs.expo.dev/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config' );
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add support for @ alias
+config.resolver.extraNodeModules = {
+  '@': path.resolve(__dirname),
+};
 
 module.exports = config;
